60e6db37c8a825b9bb5ee3a7cc77d2c35be3e9f6
Stanislav Dyagilev <su-27sm1@rambler.ru>
2022-12-21 21:56:50 +0300

    Importing { Command } from commander in bin/ gendiff.js Adding node_modules in .gitignore Updating package.json by adding "type": "module" and path to bin/gendiff Updating dependencies commander

---
+++ bin/gendiff.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+import { Command } from 'commander';
+const program = new Command();
+program
+  .version('0.1.0')
+  .description('Compares two configuration files and shows a difference.');
+program.parse();
