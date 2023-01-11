2b39f2aafb2df89c938da14a7630ada259666f11
Stanislav Dyagilev <su-27sm1@rambler.ru>
2023-01-11 17:09:18 +0300

    Add testing into index.test.js

--- _tests_/index.test.js
+++ _tests_/index.test.js
@@ -14,16 +14,34 @@ const getFixturePath = (filename) =>
 
 const file1 = getFixturePath('file1.json');
 const file2 = getFixturePath('file2.json');
-// const file3 = getFixturePath('file1.yaml');
-// const file4 = getFixturePath('file2.yaml');
-// const file5 = getFixturePath('file1.yml');
-// const file6 = getFixturePath('file2.yml');
+const file3 = getFixturePath('file1.yaml');
+const file4 = getFixturePath('file2.yaml');
+const file5 = getFixturePath('file1.yml');
+const file6 = getFixturePath('file2.yml');
 
-const expected = fs.readFileSync(getFixturePath('result_json.json'), 'utf-8');
+const expected1 = fs.readFileSync(getFixturePath('result_stylish.txt'), 'utf8');
 
-test('difference test', () => {
-  expect(genDiff(file1, file2)).toEqual(expected);
-  // expect(genDiff(file3, file4)).toEqual(expected);
-  // expect(genDiff(file5, file6)).toEqual(expected);
-  // expect(genDiff(file1, file6)).toEqual(expected);
+test('Stylish test', () => {
+  expect(genDiff(file1, file2)).toEqual(expected1);
+  expect(genDiff(file3, file4)).toEqual(expected1);
+  expect(genDiff(file5, file6)).toEqual(expected1);
+  expect(genDiff(file1, file6)).toEqual(expected1);
+});
+
+const expected2 = fs.readFileSync(getFixturePath('result_plain.txt'), 'utf8');
+
+test('Plain test', () => {
+  expect(genDiff(file1, file2, 'plain')).toEqual(expected2);
+  expect(genDiff(file3, file4, 'plain')).toEqual(expected2);
+  expect(genDiff(file5, file6, 'plain')).toEqual(expected2);
+  expect(genDiff(file1, file6, 'plain')).toEqual(expected2);
+});
+
+const expected3 = fs.readFileSync(getFixturePath('result_json.json'), 'utf8');
+
+test('Json test', () => {
+  expect(genDiff(file1, file2, 'json')).toEqual(expected3);
+  expect(genDiff(file3, file4, 'json')).toEqual(expected3);
+  expect(genDiff(file5, file6, 'json')).toEqual(expected3);
+  expect(genDiff(file1, file6, 'json')).toEqual(expected3);
 });
